0aea7e55397ed77a9709aeac8363710c283575d1
kaebn <suhbat8575@gmail.com>
2023-05-17 18:48:15 +0800

    main hesegt gallery hiiw

---
+++ public/components/Gallery/js/app.js
@@ -0,0 +1,48 @@
+function provinces() {
+    window.open("https://en.wikipedia.org/wiki/Provinces_of_Mongolia");
+}
+function language() {
+    window.open("https://en.wikipedia.org/wiki/Mongolian_language");
+}
+function city() {
+    window.open("https://en.wikipedia.org/wiki/Ulaanbaatar");
+}
+function horses() {
+    window.open("https://en.wikipedia.org/wiki/Mongolian_horse");
+}
+function size() {
+ window.open("https://en.wikipedia.org/wiki/Mongolia")
+}
+function currency() {
+    window.open("https://en.wikipedia.org/wiki/Mongolian_t%C3%B6gr%C3%B6g");
+}
+function gobi() {
+    window.open("https://en.wikipedia.org/wiki/Gobi_Desert#European_exploration"); 
+}
+function population() {
+    window.open("https://en.wikipedia.org/wiki/Demographics_of_Mongolia");
+}
+function liveStock() {
+    window.open("https://montsame.mn/en/read/211029");
+}
+function president() {
+    window.open("https://en.wikipedia.org/wiki/President_of_Mongolia");
+}
+function taihar() {
+    window.open("https://www.atlasobscura.com/places/taikhar-rock");
+}
+function otgonTenger() {
+    window.open("https://en.wikipedia.org/wiki/Otgontenger");
+}
+function khorgiinTogoo() {
+    window.open("https://www.zendmentravel.com/khorgo-volcano-horgiin-togoo-mongolia/");
+}
+function khuvsgul(){
+    window.open("https://en.wikipedia.org/wiki/Lake_Kh%C3%B6vsg%C3%B6l");
+}
+function tsonjin() {
+    window.open("https://en.wikipedia.org/wiki/Equestrian_statue_of_Genghis_Khan");
+}
+function ulaanTsutgalan() {
+    window.open("https://en.wikipedia.org/wiki/Orkhon_River");
+}
